3e521b02718e2cac6ee6b7a7e5865d054e59b3ea
Catalyst Students <accounts@catalystclass.com>
2013-02-16 14:22:41 -0800

    Moved add function out of maker so that the function can be shared between instances of the object.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+/*
+The maker function that returns instances of the word object.
+If an argument is given, the letters property will be initialized
+to that. Else, the letters property is initialized to an empty
+string.
+*/
+
 var makeWord = function (initialLetters) {
   var word = {
     letters : initialLetters || "",
-    add : function (lettersToAdd) {
-      letters += lettersToAdd;
-    }
+    add : add
   };
   return word;
-}
+};
 
-var testWord = makeWord();
+var add = function (lettersToAdd) {
+  this.letters += lettersToAdd;
+};
+
+//Testing the function.
+var testWord = makeWord("What up cuz.");
 alert(testWord.letters);
